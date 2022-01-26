e67c06fcc88b2a878766aac68a1705cbf57dee62
huu kien <huukien0099@gmail.com>
2022-01-26 21:23:44 +0700

    gg

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use("/api/products", productRouter);
 
 app.use("/api/orders", orderRouter);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log("Backend server is running!");
 });
